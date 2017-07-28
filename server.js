998d9d80737b2427504e7642049b85d5e02a709b
Robby Bourne <robbybourne1@gmail.com>
2017-07-28 10:28:06 -0400

    Soemthings in the Water

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const mustcheExpress = require('mustache-express')
+const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+
+const app = express()
 
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
@@ -11,3 +13,16 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
+
+//
+// const wordGuess = {
+//   letter:
+// }
+
+app.get('/', (request, response) => {
+  response.send('Hello!!')
+})
+
+app.listen(3000, () => {
+  console.log('Somethings in the water!!!')
+})
